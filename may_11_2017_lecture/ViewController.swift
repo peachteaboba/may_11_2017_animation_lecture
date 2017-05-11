5bfe66ec44f35f269ad8838d2bb28e8825b84e8d
Andy Feng <anndyfeng1@gmail.com>
2017-05-11 10:48:44 -0700

    fc

--- may_11_2017_lecture/ViewController.swift
+++ may_11_2017_lecture/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /// This need work...
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var menuViewLeadingConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /// Create the event handler constant
+        let myLabelTap = UITapGestureRecognizer(target: self, action: #selector(handleMyLabelTapped))
+        
+        /// Enable actions on the label
+        myLabel.isUserInteractionEnabled = true
+        
+        /// Assign the event handler to our label
+        myLabel.addGestureRecognizer(myLabelTap)
+        
+        
+        
+        menuViewLeadingConstraint.constant = -300
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func handleMyLabelTapped(){
+        print("label was tapped!")
+        
+        if menuViewLeadingConstraint.constant != -50 {
+            menuViewLeadingConstraint.constant = -50
+        } else {
+            menuViewLeadingConstraint.constant = -300
+        }
+        
+        
+        /// Code to start animation =======================================
+        self.view.setNeedsLayout()
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.45, initialSpringVelocity: 0.7, options: [UIViewAnimationOptions.allowUserInteraction], animations: {
+        
+            // Code inside animation function
+            self.view.layoutIfNeeded()
+            
+        })
+        /// ===============================================================
+    }
+    
+    
 
 }
 
